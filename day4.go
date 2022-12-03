e9f5a18df6776732b668363b85f519c1feedc73f
Jurrien Fakkeldij <jurrien.fakkeldij@gmail.com>
2022-12-03 23:17:09 +0100

    prepare day 4

---
+++ day4.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Day4() {
+	fmt.Println("============== Day4 ==============")
+	Day4Part1()
+	Day4Part2()
+}
+
+func Day4Part1() {
+	fmt.Println("============== Part1 =============")
+	//inventory := filereader.ReadFile("./input/day-4/test.txt")
+
+	score := 0
+
+	if score != 157 {
+		fmt.Println("Test went wrong:", score)
+	} else {
+		fmt.Println("Test passed:", score)
+	}
+
+	//inventory = filereader.ReadFile("./input/day-4/input.txt")
+	score = 0
+
+	fmt.Println("Output:", score)
+}
+
+func Day4Part2() {
+	fmt.Println("============== Part2 =============")
+	//inventory := filereader.ReadFile("./input/day-4/test.txt")
+
+	score := 0
+
+	if score != 70 {
+		fmt.Println("Test went wrong:", score)
+	} else {
+		fmt.Println("Test passed:", score)
+	}
+
+	//inventory = filereader.ReadFile("./input/day-4/input.txt")
+	score = 0
+	fmt.Println("Output:", score)
+}
