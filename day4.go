b5a7a4601c9e1de13d47c13ec5275bfc40c8a21a
Jurrien Fakkeldij <jurrien.fakkeldij@gmail.com>
2022-12-05 17:37:53 +0100

    day 4

--- day4.go
+++ day4.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"internal/filereader"
+	"strconv"
+	"strings"
 )
 
 func Day4() {
@@ -12,35 +15,90 @@ func Day4() {
 
 func Day4Part1() {
 	fmt.Println("============== Part1 =============")
-	//inventory := filereader.ReadFile("./input/day-4/test.txt")
+	assignments := filereader.ReadFile("./input/day-4/test.txt")
 
-	score := 0
+	noOfAssignments := findContainingAssigments(assignments)
 
-	if score != 157 {
-		fmt.Println("Test went wrong:", score)
+	if noOfAssignments != 2 {
+		fmt.Println("Test went wrong:", noOfAssignments)
 	} else {
-		fmt.Println("Test passed:", score)
+		fmt.Println("Test passed:", noOfAssignments)
 	}
 
-	//inventory = filereader.ReadFile("./input/day-4/input.txt")
-	score = 0
+	assignments = filereader.ReadFile("./input/day-4/input.txt")
+	noOfAssignments = findContainingAssigments(assignments)
 
-	fmt.Println("Output:", score)
+	fmt.Println("Output:", noOfAssignments)
+}
+
+func findContainingAssigments(assignments []string) int {
+	countContainingAssignments := 0
+	for _, assignment := range assignments {
+		pairs := strings.Split(assignment, ",")
+		firstPairFirstAssi, _ := strconv.Atoi(strings.Split(pairs[0], "-")[0])
+		firstPairSecondAssi, _ := strconv.Atoi(strings.Split(pairs[0], "-")[1])
+		secondPairFirstAssi, _ := strconv.Atoi(strings.Split(pairs[1], "-")[0])
+		secondPairSecondAssi, _ := strconv.Atoi(strings.Split(pairs[1], "-")[1])
+
+		if firstPairFirstAssi <= secondPairFirstAssi &&
+			firstPairSecondAssi >= secondPairSecondAssi {
+			countContainingAssignments++
+		} else if firstPairFirstAssi >= secondPairFirstAssi &&
+			firstPairSecondAssi <= secondPairSecondAssi {
+			countContainingAssignments++
+		}
+	}
+
+	return countContainingAssignments
 }
 
 func Day4Part2() {
 	fmt.Println("============== Part2 =============")
-	//inventory := filereader.ReadFile("./input/day-4/test.txt")
+	assignments := filereader.ReadFile("./input/day-4/test.txt")
 
-	score := 0
+	noOfAssignments := findOverlappingAssigments(assignments)
 
-	if score != 70 {
-		fmt.Println("Test went wrong:", score)
+	if noOfAssignments != 4 {
+		fmt.Println("Test went wrong:", noOfAssignments)
 	} else {
-		fmt.Println("Test passed:", score)
+		fmt.Println("Test passed:", noOfAssignments)
 	}
 
-	//inventory = filereader.ReadFile("./input/day-4/input.txt")
-	score = 0
-	fmt.Println("Output:", score)
+	assignments = filereader.ReadFile("./input/day-4/input.txt")
+	noOfAssignments = findOverlappingAssigments(assignments)
+
+	fmt.Println("Output:", noOfAssignments)
+}
+
+func findOverlappingAssigments(assignments []string) int {
+
+	countOverlappingAssignments := 0
+	for _, assignment := range assignments {
+		pairs := strings.Split(assignment, ",")
+		firstPairFirstAssi, _ := strconv.Atoi(strings.Split(pairs[0], "-")[0])
+		firstPairSecondAssi, _ := strconv.Atoi(strings.Split(pairs[0], "-")[1])
+		secondPairFirstAssi, _ := strconv.Atoi(strings.Split(pairs[1], "-")[0])
+		secondPairSecondAssi, _ := strconv.Atoi(strings.Split(pairs[1], "-")[1])
+
+		if firstPairFirstAssi <= secondPairFirstAssi &&
+			firstPairSecondAssi >= secondPairSecondAssi {
+			countOverlappingAssignments++
+		} else if firstPairFirstAssi >= secondPairFirstAssi &&
+			firstPairSecondAssi <= secondPairSecondAssi {
+			countOverlappingAssignments++
+		} else if firstPairFirstAssi <= secondPairFirstAssi && // 1-10, 2-12
+			firstPairSecondAssi >= secondPairFirstAssi {
+			countOverlappingAssignments++
+		} else if secondPairSecondAssi <= firstPairSecondAssi && //6-10, 3-9
+			secondPairSecondAssi >= firstPairFirstAssi {
+			countOverlappingAssignments++
+		} else if firstPairFirstAssi >= secondPairFirstAssi && //6-10, 1-7
+			firstPairSecondAssi <= secondPairFirstAssi {
+			countOverlappingAssignments++
+		} else if secondPairSecondAssi >= firstPairSecondAssi &&
+			secondPairSecondAssi <= firstPairFirstAssi {
+			countOverlappingAssignments++
+		}
+	}
+	return countOverlappingAssignments
 }
