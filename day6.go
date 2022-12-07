bc308f7c1b7de8f892f9cf62bd05e4750d2a172e
Jurrien Fakkeldij <jurrien.fakkeldij@gmail.com>
2022-12-07 09:14:31 +0100

    day6

--- day6.go
+++ day6.go
@@ -3,46 +3,114 @@ package main
 import (
 	"fmt"
 	"internal/filereader"
+	"strings"
 )
 
 func Day6() {
 	fmt.Println("============== Day6 ==============")
-	Day5Part1()
-	Day5Part2()
+	Day6Part1()
+	Day6Part2()
 }
 
 func Day6Part1() {
 	fmt.Println("============== Part1 =============")
-	cargoAndProcedures := filereader.ReadFile("./input/day-5/test.txt")
+	instructions := filereader.ReadFile("./input/day-6/test.txt")
 
-	items := findTopItems(cargoAndProcedures)
+	marker, position := findMarker(instructions[0])
 
-	if items != "CMZ" {
-		fmt.Println("Test went wrong:", items)
+	if marker != "jpqm" && position == 7 {
+		fmt.Println("Test went wrong:", marker, position)
 	} else {
-		fmt.Println("Test passed:", items)
+		fmt.Println("Test passed:", marker, position)
 	}
 
-	cargoAndProcedures = filereader.ReadFile("./input/day-5/input.txt")
-	items = findTopItems(cargoAndProcedures)
+	instructions = filereader.ReadFile("./input/day-6/input.txt")
+	marker, position = findMarker(instructions[0])
 
-	fmt.Println("Output:", items)
+	fmt.Println("Output:", marker, position)
+}
+
+func findMarker(instructions string) (string, int) {
+	startPosition := 2
+	markerSet := instructions[:startPosition+1]
+	markerPosition := 0
+	noDuplicateChars := false
+	for i := startPosition + 1; i < len(instructions); i++ {
+		markerSet += string(instructions[i])
+		for j := 0; j < len(markerSet); j++ {
+			newMarkerSet := removeDuplicateValues(markerSet)
+			if strings.Compare(newMarkerSet, markerSet) != 0 {
+				//fmt.Println(markerSet, i, string(character))
+				markerSet = markerSet[1:]
+				//fmt.Println(markerSet, i)
+				break
+			} else {
+				noDuplicateChars = true
+			}
+		}
+
+		if noDuplicateChars {
+			markerPosition = i + 1
+			break
+		}
+	}
+	return markerSet, markerPosition
+}
+
+func removeDuplicateValues(line string) string {
+	keys := make(map[string]bool)
+	list := ""
+
+	for _, entry := range line {
+		if _, value := keys[string(entry)]; !value {
+			keys[string(entry)] = true
+			list += string(entry)
+		}
+	}
+	return list
 }
 
 func Day6Part2() {
 	fmt.Println("============== Part2 =============")
-	cargoAndProcedures := filereader.ReadFile("./input/day-5/test.txt")
+	instructions := filereader.ReadFile("./input/day-6/test.txt")
 
-	items := findTopItems9001(cargoAndProcedures)
+	message, position := findMessage(instructions[0])
 
-	if items != "MCD" {
-		fmt.Println("Test went wrong:", items)
+	if position != 19 {
+		fmt.Println("Test went wrong:", message, position)
 	} else {
-		fmt.Println("Test passed:", items)
+		fmt.Println("Test passed:", message, position)
 	}
 
-	cargoAndProcedures = filereader.ReadFile("./input/day-5/input.txt")
-	items = findTopItems9001(cargoAndProcedures)
+	instructions = filereader.ReadFile("./input/day-6/input.txt")
+	message, position = findMessage(instructions[0])
 
-	fmt.Println("Output:", items)
+	fmt.Println("Output:", message, position)
+}
+
+func findMessage(instructions string) (string, int) {
+	startPosition := 12
+	markerSet := instructions[:startPosition+1]
+	markerPosition := 0
+	noDuplicateChars := false
+	for i := startPosition + 1; i < len(instructions); i++ {
+		markerSet += string(instructions[i])
+		for j := 0; j < len(markerSet); j++ {
+			newMarkerSet := removeDuplicateValues(markerSet)
+			if strings.Compare(newMarkerSet, markerSet) != 0 {
+				//fmt.Println(markerSet, i, string(character))
+				markerSet = markerSet[1:]
+				//fmt.Println(markerSet, i)
+				break
+			} else {
+				noDuplicateChars = true
+			}
+		}
+
+		if noDuplicateChars {
+			markerPosition = i + 1
+			break
+		}
+	}
+	return markerSet, markerPosition
 }
